470431f32b361f1b7624426f9ad4491cb48dbd74
raghukapur9 <64493087+raghukapur9@users.noreply.github.com>
2022-02-24 12:39:43 +0530

    basics for modifiers and payment

---
+++ hotel_room.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.7;
+
+contract HotelRoom{
+
+    address payable public  owner;
+    enum Statuses {
+        Vacant,
+        Occupied
+    }
+    Statuses currentStatus;
+    event Occupy(address _occupant, uint _amount);
+    constructor() public {
+        owner = msg.sender;
+        currentStatus = Statuses.Vacant;
+    }
+    modifier OnlyWhileVacant{
+        require(currentStatus == Statuses.Vacant, "Currently Occupied");
+        _;
+    }
+
+    modifier EnoughEtherSent(uint _amount) {
+        require((msg.value >= _amount), "Not enough ether provided");
+        _;
+    }
+    function bookRoom() payable public OnlyWhileVacant EnoughEtherSent(2 ether){
+        currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
+    }
+    
+}
